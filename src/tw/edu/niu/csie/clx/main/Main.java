32d4403fa543dca9926d5b0893541bc781063171
Li-Xian Chen <b0543017@ems.niu.edu.tw>
2019-06-16 22:39:18 +0800

    cleanup Main.java

--- src/tw/edu/niu/csie/clx/main/Main.java
+++ src/tw/edu/niu/csie/clx/main/Main.java
@@ -1,21 +1,13 @@
 package tw.edu.niu.csie.clx.main;
 
 import tw.edu.niu.csie.clx.gui.MainWindow;
-import tw.edu.niu.csie.clx.network.Client;
-import tw.edu.niu.csie.clx.network.WebSocketManager;
-
-import java.awt.*;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class Main {
 
     public static final String WS_SERVER_URL = "wss://project-rmc.herokuapp.com/ws";
     public static final String SERVER_BASE_URL = "https://project-rmc.herokuapp.com/control/";
 
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) {
         new MainWindow();
-//        Client client = new Client();
-//        client.connect();
     }
 }
